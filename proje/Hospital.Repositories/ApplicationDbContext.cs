6fe82106f7739030f1a72cb1acf78a4f928d94cb
Berkant <berkant.ontemel@ogr.sakarya.edu.tr>
2023-12-10 22:20:54 +0300

    p-1
    Gerekli tanımlamalar, bağlamlar yapıldı. sürümler 6.08 Utilis ve repositories de yazılımsal olmayan probelmler mevcut. Database bağlantısı yapılıyor.

---
+++ proje/Hospital.Repositories/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Repositories
+{
+    public class ApplicationDbContext :  IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+            
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
