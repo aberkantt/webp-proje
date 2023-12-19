f15af34cadc26d0d4ccbfcf7a8b17702133d66f5
handeakturk <133210761+handeakturk@users.noreply.github.com>
2023-12-19 20:28:50 +0300

    P-8
    Doktor ve hasta ekleme sayfaları eklendi, DB'deki problemler çözüldü
    
    Co-Authored-By: Berkant <85020893+aberkantt@users.noreply.github.com>

---
+++ proje (1)/proje/Hospital.ViewModels/ContactViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Hospital.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.ViewModels
+{
+    public class ContactViewModel
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int HospitalId { get; set; }
+
+        public ContactViewModel(Contact model) { }
+
+        public ContactViewModel(ContactViewModel model) 
+        {
+            Id= model.Id;
+            Email= model.Email;
+            Phone= model.Phone;
+            HospitalId= model.HospitalId;
+        }
+        public Contact ConvertViewModel(ContactViewModel model)
+        {
+            return new Contact
+            {
+                Id = model.Id,
+                Email = model.Email,
+                Phone = model.Phone,
+                HospitalId = model.HospitalId,
+            };
+            
+        }
+    }
+}
